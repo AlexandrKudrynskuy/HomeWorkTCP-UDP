0b402f9e59d062f61e8b569e73cbb8e8105b484a
AlexandrKudrynskuy <99129020+AlexandrKudrynskuy@users.noreply.github.com>
2023-03-30 11:29:43 +0300

    Voice Message UDP

---
+++ 2/ClientMicroPhone/ClientMP/ClientMP/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Button = System.Windows.Controls.Button;
+using Window = System.Windows.Window;
+
+namespace ClientMP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<string> Records { get; set; }
+        private NAudio.Wave.WaveFileReader wave;
+        private NAudio.Wave.DirectSoundOut output;
+        private UdpClient client;
+        private IPEndPoint endPoint;
+        public MainWindow()
+        {
+            InitializeComponent();
+            wave = null;
+            output = null;
+            client = new UdpClient(2424);
+            endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.255"), 2424);
+        }
+
+        private void PlayBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+
+                wave = new NAudio.Wave.WaveFileReader(button.Tag.ToString());
+                output = new NAudio.Wave.DirectSoundOut();
+                output.Init(new NAudio.Wave.WaveChannel32(wave));
+                output.Play();
+            }
+        }
+
+        private void StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                Records.Remove(button.Tag.ToString());
+                System.IO.File.Delete(button.Tag.ToString());
+            }
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Records = new ObservableCollection<string>();
+            var files = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\" + "record");
+            foreach (var file in files)
+            {
+                Records.Add(file);
+            }
+            recordListView.ItemsSource = Records;
+
+
+            await Task.Run(() =>
+            {
+            while (true)
+
+            {
+                byte[] res = client.Receive(ref endPoint);
+                string path = Directory.GetCurrentDirectory() + "\\" + "record" + "\\" + Guid.NewGuid() + ".wav";
+                System.IO.File.WriteAllBytes(path, res);
+                Dispatcher.BeginInvoke(() => {
+                    Records.Add(path);
+                });
+
+                }
+            });
+
+
+
+
+
+        }
+    }
+}
---
+++ 2/RecordMicroPhone/RecordMicroPhone/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RecordMicroPhone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WaveIn waveIn;
+        private WaveFileWriter writer;
+        private string outputFilename= Guid.NewGuid()+"1.wav";
+        private UdpClient server;
+        private IPEndPoint endPoint;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            server = new UdpClient();
+            endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.255"),2424);
+        }
+
+        private void WaveIn_RecordingStopped(object? sender, StoppedEventArgs e)
+        {
+            waveIn.Dispose();
+            waveIn = null;
+            writer.Close();
+            writer = null;
+
+        }
+
+        private void WaveIn_DataAvailable(object? sender, WaveInEventArgs e)
+        {
+           
+                var res = e.Buffer;
+                writer.WriteData(e.Buffer, 0, e.BytesRecorded);
+
+
+        }
+        private void RecordButton_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                waveIn = new WaveIn();
+           
+                waveIn.DeviceNumber = 0;
+                waveIn.DataAvailable += WaveIn_DataAvailable; ;
+                waveIn.RecordingStopped += WaveIn_RecordingStopped; ;
+                writer = new WaveFileWriter(outputFilename, waveIn.WaveFormat);
+                RecordLabel.Content = "Start Recording";
+                waveIn.StartRecording();
+            }
+            catch (Exception ex)
+            { 
+                MessageBox.Show(ex.Message); 
+            }
+           
+        }
+        private void RecordButton_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            waveIn.StopRecording();
+
+            if (waveIn != null)
+            {
+                RecordLabel.Content = "Recording finich";
+            }
+        }
+
+        private async void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            var record = File.ReadAllBytes(outputFilename);
+            server.SendAsync(record, record.Length, endPoint);
+        }
+    }
+}
